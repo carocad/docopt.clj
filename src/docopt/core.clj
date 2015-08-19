a250aa66eb555d35d228c5602beae57a7e7352b4
Camilo Roca <carocad@unal.edu.co>
2015-08-19 22:08:07 +0200

    java interoperability deleted. require now with more meaninful names parse renamed to parse-docstring docopt now is a function not a macro

--- src/docopt/core.clj
+++ src/docopt/core.clj
@@ -1,42 +1,26 @@
-(ns docopt.core  
-  (:require [clojure.string      :as s])
-  (:require [docopt.match        :as m])
-  (:require [docopt.optionsblock :as o])
-  (:require [docopt.usageblock   :as u])
+(ns docopt.core
+  (:require [clojure.string      :as string])
+  (:require [docopt.match        :as match])
+  (:require [docopt.optionsblock :as option])
+  (:require [docopt.usageblock   :as usage])
   (:require [docopt.util         :as util])
-  (:import java.util.HashMap)
-  (:gen-class
-    :name org.docopt.clj
-    :methods [^{:static true} [docopt [String "[Ljava.lang.String;"] java.util.AbstractMap]]))
+  (:import java.util.HashMap))
 
-(defn parse
+(defn parse-docstring
   "Parses doc string."
   [doc]
-  {:pre [(string? doc)]}
   (letfn [(sec-re   [name]          (re-pattern (str "(?:^|\\n)(?!\\s).*(?i)" name ":\\s*(.*(?:\\n(?=[ \\t]).+)*)")))
-          (section  [name splitfn]  (map s/trim (mapcat (comp splitfn second) (re-seq (sec-re name) doc))))
+          (section  [name splitfn]  (map string/trim (mapcat (comp splitfn second) (re-seq (sec-re name) doc))))
           (osplitfn [options-block] (re-seq #"(?<=^|\n)\s*-.*(?:\s+[^- \t\n].*)*" options-block))]
-    (u/parse (section "usage" s/split-lines) (o/parse (section "options" osplitfn)))))
+    (usage/parse (section "usage" string/split-lines) (option/parse (section "options" osplitfn)))))
 
-(defmacro docopt
+(defn docopt
   "Parses doc string at compile-time and matches command line arguments at run-time.
 The doc string may be omitted, in which case the metadata of '-main' is used"
-  ([args] 
+  ([args]
     (let [doc (:doc (meta (find-var (symbol (pr-str (ns-name *ns*)) "-main"))))]
       (if (string? doc)
-        `(m/match-argv ~(parse doc) ~args)
+        (match/match-argv (parse-docstring doc) args)
         (throw (Exception. "Docopt with one argument requires that #'-main have a doc string.\n")))))
   ([doc args]
-    `(m/match-argv ~(parse doc) ~args)))
-
-(defn -docopt 
-  "Java-capable run-time equivalent to 'docopt';
-argument 'doc' can be either a doc string or the result of a call to 'parse'.
-Returns a java.util.HashMap of the matched values provided by the 'args' sequence."
-  [doc args]
-  (if-let [cljmap (m/match-argv (if (string? doc) (parse doc) doc) (into [] args))]
-    (let [javamap (HashMap. (count cljmap))]
-      (doseq [[k v] cljmap]
-        (.put javamap k (if (vector? v) (into-array v) v)))
-      javamap)))
-
+    (match/match-argv (parse-docstring doc) args)))
