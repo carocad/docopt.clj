51af69fcfcfeef8bfdba2eff43c9ef7a09d642a9
Camilo Roca <carocad@unal.edu.co>
2015-08-24 20:42:59 +0200

    parse2 and tokenize functions created as replacement of old functions. old functions not deleted YET

--- src/docopt/optionsblock.clj
+++ src/docopt/optionsblock.clj
@@ -1,18 +1,48 @@
 (ns docopt.optionsblock
   (:require [clojure.string :as string])
-  (:use      docopt.util))
+  (:require [docopt.util :as util]))
 
-(defn tokenize-option
+(defn parse2
+  [option-lines]
+  (let [tokenized-options (map tokenize options-lines)]
+    ;REVIEW: Is there a more clean way to check this?
+    (assert (and (distinct? (filter identity (map :long tokenized-options)))
+                 (distinct? (filter identity (map :short tokenized-options))))
+            "SYNTAX ERROR: In options descriptions, at least one option defined
+            more than once.")))
+
+(defn- tokenize ; wait until you can replace the complete function below
+  "Generates a sequence of tokens for an option specification string."
+  [line] ; second drops the first match, usually a trailing Character -, --
+  (hash-map :default (second (re-find (:default option-types) line))
+            :short   (second (re-find (:short option-types) line))
+            :long    (second (re-find (:long option-types) line))
+            :arg     (first (re-find (:arg option-types) line))))
+
+; REVIEW: do we really need this?
+(defn takes-arg?
+  [{:keys [arg]}]
+  (true? arg))
+
+; REVIEW: should this be really public?
+(def option-types {:default   #"\s{2,}(?s).*\[(?i)default(?-i):\s*([^\]]+).*"
+                   :short     #"(?:^|\s+),?\s*-([^-,])"
+                   :long      #"(?:^|\s+),?\s*--([^ \t=,]+)"
+                   :arg       #"(<[^<>]*>|[A-Z_0-9]*[A-Z_][A-Z_0-9]*\s)"})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- tokenize-option
   "Generates a sequence of tokens for an option specification string."
   [string]
-  (tokenize string [[#"\s{2,}(?s).*\[(?i)default(?-i):\s*([^\]]+).*" :default]
+  (util/tokenize string [[#"\s{2,}(?s).*\[(?i)default(?-i):\s*([^\]]+).*" :default]
                     [#"\s{2,}(?s).*"] ; description of the option
                     [#"(?:^|\s+),?\s*-([^-,])"                       :short]
                     [#"(?:^|\s+),?\s*--([^ \t=,]+)"                  :long]
                     [(re-pattern re-arg-str)                         :arg]
                     [#"\s*[=,]?\s*"]])) ; UNKNOWN
 
-(defn parse-option
+(defn- parse-option
   "Parses option description line into associative map."
   [option-line]
   (let [tokens (tokenize-option option-line)]
