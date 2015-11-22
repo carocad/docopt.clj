866b549fc35db6adcd7db9cd5bdd358a0a26151d
Camilo Roca <carocad@unal.edu.co>
2015-11-22 22:23:39 +0100

    project.clj adapted to clojar requirements

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject docopt "0.6.1"
-  :description "docopt creates beautiful command-line interfaces - clojure port"
-  :url "http://docopt.org"
+(defproject org.clojars.carocad/docopt "0.1.0"
+  :description "Docopt creates beautiful command-line interfaces - clojure (unnoficial) port"
+  :url "https://github.com/carocad/docopt.cluno"
   :license {:name "MIT" :url "https://raw.githubusercontent.com/carocad/docopt.cluno/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [instaparse "1.4.1"]]
   :profiles {:test {:dependencies [[org.clojure/data.json "0.2.1"]]}}
+  ;:main ^:skip-aot docopt.main
   :aot :all)
