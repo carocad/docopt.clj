bf787411c2f39e0962efe65220c2325d9babe29f
Camilo Roca <carocad@unal.edu.co>
2017-03-14 18:59:53 -0500

    use latest clojure version

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject org.clojars.carocad/docopt "0.1.0"
   :description "Docopt creates beautiful command-line interfaces - clojure (unnoficial) port"
   :url "https://github.com/carocad/docopt.cluno"
-  :license {:name "MIT" :url "https://raw.githubusercontent.com/carocad/docopt.cluno/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :license {:name "MIT"
+            :url "https://github.com/carocad/docopt.cluno/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [instaparse "1.4.1"]]
-  :profiles {:test {:dependencies [[org.clojure/data.json "0.2.1"]]}}
-  ;:main ^:skip-aot docopt.main
-  :aot :all)
+  :profiles {:test {:dependencies [[org.clojure/data.json "0.2.1"]]}})
