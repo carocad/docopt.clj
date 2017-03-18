87a467316401f7c086e3c5eca8d78930dda1e877
Camilo Roca <carocad@unal.edu.co>
2017-03-18 18:29:42 -0500

    version bumped

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.carocad/docopt "0.1.0"
+(defproject org.clojars.carocad/docopt "0.2.0"
   :description "Docopt creates beautiful command-line interfaces - clojure (unnoficial) version"
   :url "https://github.com/carocad/docopt.clj"
   :plugins [[jonase/eastwood "0.2.3"]]
