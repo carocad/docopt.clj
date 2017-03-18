23022ff8a58a0a92a6a0d7b1bf251bbe18cebdea
Camilo Roca <carocad@unal.edu.co>
2017-03-18 14:16:03 -0500

    eastwood plugin added

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.clojars.carocad/docopt "0.1.0"
   :description "Docopt creates beautiful command-line interfaces - clojure (unnoficial) version"
   :url "https://github.com/carocad/docopt.clj"
+  :plugins [[jonase/eastwood "0.2.3"]]
   :license {:name "MIT"
             :url "https://github.com/carocad/docopt.clj/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
